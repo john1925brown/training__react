4a2985c2c1b165ea8ae6761685354a410d47792f
Dzmitry Yarasheuski <materik08082020@gmail.com>
2024-12-18 04:24:18 +0300

    localStorage training

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import { Accordeon } from './components/Accordeon';
 import { Radio } from './components/Radio/Radio';
 import { CashMashine } from './components/CashMashine/CashMashine';
+import { Storage } from './components/LocalStrorageTrain/Storage';
 
 function App() {
   return (
     <div className="App">
       {/* <Accordeon title="My list" /> */}
       {/* <Radio /> */}
-      <CashMashine />
+      {/* <CashMashine /> */}
+      <Storage/>
     </div>
   );
 }

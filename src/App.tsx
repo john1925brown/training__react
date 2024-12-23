9597d69de0992b5996cdda1712f6434907c3e916
Dzmitry Yarasheuski <materik08082020@gmail.com>
2024-12-23 21:09:48 +0300

    storyBook basics

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Accordeon } from './components/Accordeon';
 import { Radio } from './components/Radio/Radio';
 import { CashMashine } from './components/CashMashine/CashMashine';
 import { Storage } from './components/LocalStrorageTrain/Storage';
 import AssotiativeArray from './components/associativeArray/src/AssotiativeArray';
+import { Accordion } from './components/Accordion';
 
 function App() {
   return (
     <div className="App">
-      {/* <Accordeon title="My list" /> */}
+      <Accordion title="My list" />
       {/* <Radio /> */}
       {/* <CashMashine /> */}
       {/* <Storage/> */}
-      <AssotiativeArray/>
+      {/* <AssotiativeArray /> */}
     </div>
   );
 }

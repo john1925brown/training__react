79f4f2f2029e83e840a630f4bc6f2fd83e889589
Dzmitry Yarasheuski <materik08082020@gmail.com>
2025-01-03 03:08:16 +0300

    flux part 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Radio } from './components/Radio/Radio';
 import { CashMashine } from './components/CashMashine/CashMashine';
 import { Storage } from './components/LocalStrorageTrain/Storage';
 import AssotiativeArray from './components/associativeArray/src/AssotiativeArray';
 import { Accordion } from './components/Accordion';
+import { ControlledInput } from './components/ControlledInput/ControlledInput.stories';
 
 function App() {
   return (

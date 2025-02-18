306154abbbe0a59a190f91d96d15e8421d1c1139
Dzmitry Yarasheuski <materik08082020@gmail.com>
2025-02-18 19:22:21 +0300

    useSearchPArams

--- src/router_dom/src/AppRouter.tsx
+++ src/router_dom/src/AppRouter.tsx
@@ -5,11 +5,13 @@ import { Puma } from './components/pages/Puma';
 import { PageThree } from './components/pages/PageThree';
 import { Error404 } from './components/pages/Error404';
 import { Model } from './components/pages/Model';
+import { Prices } from './components/pages/Prices';
 
 export const PATH = {
   ADIDAS: '/adidas',
   PUMA: '/puma',
   PAGE3: '/abibas',
+  PRICES: '/prices',
   ERROR404: '/error404',
 } as const;
 
@@ -51,6 +53,16 @@ export function AppRouter() {
               Abibas
             </NavLink>
           </div>
+          <div>
+            <NavLink
+              className={({ isActive }) =>
+                isActive ? styles.activeNavLink : styles.navLink
+              }
+              to={PATH.PRICES}
+            >
+              Prices
+            </NavLink>
+          </div>
         </div>
         <div className={styles.content}>
           <Routes>
@@ -59,13 +71,9 @@ export function AppRouter() {
             <Route path={PATH.PUMA} element={<Puma />} />
             <Route path={PATH.PAGE3} element={<PageThree />} />
             <Route path={PATH.ERROR404} element={<Error404 />} />
+            <Route path={PATH.PRICES} element={<Prices />} />
 
             <Route path="/:brandName/:modelId" element={<Model />} />
-
-            {/* <Route path="/puma/:modelId" element={<Model />} /> */}
-
-            {/* <Route path="/*" element={<Navigate to={PATH.ERROR404} />} /> */}
-
           </Routes>
         </div>
       </div>
